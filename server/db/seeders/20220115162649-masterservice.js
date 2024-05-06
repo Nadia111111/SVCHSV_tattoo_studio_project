a4caffb86b75b159127658f95a743b1be7a9f088
Nadia111111 <138562665+Nadia111111@users.noreply.github.com>
2024-05-06 18:36:19 +0300

    feat: add seed for dependenciev master and service

---
+++ server/db/seeders/20220115162649-masterservice.js
@@ -0,0 +1,133 @@
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.bulkInsert('masterServices', [
+      {
+        masterId: '1', serviceId: '13', createdAt: new Date(), updatedAt: new Date(),
+      },
+      {
+        masterId: '1', serviceId: '14', createdAt: new Date(), updatedAt: new Date(),
+      },
+      {
+        masterId: '1', serviceId: '15', createdAt: new Date(), updatedAt: new Date(),
+      },
+      {
+        masterId: '2', serviceId: '13', createdAt: new Date(), updatedAt: new Date(),
+      },
+      {
+        masterId: '2', serviceId: '14', createdAt: new Date(), updatedAt: new Date(),
+      },
+      {
+        masterId: '2', serviceId: '15', createdAt: new Date(), updatedAt: new Date(),
+      },
+      {
+        masterId: '3', serviceId: '13', createdAt: new Date(), updatedAt: new Date(),
+      },
+      {
+        masterId: '3', serviceId: '14', createdAt: new Date(), updatedAt: new Date(),
+      },
+      {
+        masterId: '3', serviceId: '15', createdAt: new Date(), updatedAt: new Date(),
+      },
+      {
+        masterId: '4', serviceId: '1', createdAt: new Date(), updatedAt: new Date(),
+      },
+      {
+        masterId: '4', serviceId: '2', createdAt: new Date(), updatedAt: new Date(),
+      },
+      {
+        masterId: '4', serviceId: '3', createdAt: new Date(), updatedAt: new Date(),
+      },
+      {
+        masterId: '4', serviceId: '4', createdAt: new Date(), updatedAt: new Date(),
+      },
+      {
+        masterId: '4', serviceId: '5', createdAt: new Date(), updatedAt: new Date(),
+      },
+      {
+        masterId: '4', serviceId: '6', createdAt: new Date(), updatedAt: new Date(),
+      },
+      {
+        masterId: '4', serviceId: '7', createdAt: new Date(), updatedAt: new Date(),
+      },
+      {
+        masterId: '4', serviceId: '8', createdAt: new Date(), updatedAt: new Date(),
+      },
+      {
+        masterId: '4', serviceId: '9', createdAt: new Date(), updatedAt: new Date(),
+      },
+      {
+        masterId: '4', serviceId: '10', createdAt: new Date(), updatedAt: new Date(),
+      },
+      {
+        masterId: '4', serviceId: '11', createdAt: new Date(), updatedAt: new Date(),
+      },
+      {
+        masterId: '4', serviceId: '12', createdAt: new Date(), updatedAt: new Date(),
+      },
+      {
+        masterId: '5', serviceId: '1', createdAt: new Date(), updatedAt: new Date(),
+      },
+      {
+        masterId: '5', serviceId: '2', createdAt: new Date(), updatedAt: new Date(),
+      },
+      {
+        masterId: '5', serviceId: '3', createdAt: new Date(), updatedAt: new Date(),
+      },
+      {
+        masterId: '5', serviceId: '4', createdAt: new Date(), updatedAt: new Date(),
+      },
+      {
+        masterId: '5', serviceId: '5', createdAt: new Date(), updatedAt: new Date(),
+      },
+      {
+        masterId: '5', serviceId: '6', createdAt: new Date(), updatedAt: new Date(),
+      },
+      {
+        masterId: '5', serviceId: '7', createdAt: new Date(), updatedAt: new Date(),
+      },
+      {
+        masterId: '5', serviceId: '8', createdAt: new Date(), updatedAt: new Date(),
+      },
+      {
+        masterId: '5', serviceId: '9', createdAt: new Date(), updatedAt: new Date(),
+      },
+      {
+        masterId: '5', serviceId: '10', createdAt: new Date(), updatedAt: new Date(),
+      },
+      {
+        masterId: '5', serviceId: '11', createdAt: new Date(), updatedAt: new Date(),
+      },
+      {
+        masterId: '5', serviceId: '12', createdAt: new Date(), updatedAt: new Date(),
+      },
+      {
+        masterId: '6', serviceId: '1', createdAt: new Date(), updatedAt: new Date(),
+      },
+      {
+        masterId: '6', serviceId: '2', createdAt: new Date(), updatedAt: new Date(),
+      },
+      {
+        masterId: '6', serviceId: '3', createdAt: new Date(), updatedAt: new Date(),
+      },
+      {
+        masterId: '6', serviceId: '4', createdAt: new Date(), updatedAt: new Date(),
+      },
+      {
+        masterId: '6', serviceId: '9', createdAt: new Date(), updatedAt: new Date(),
+      },
+      {
+        masterId: '6', serviceId: '10', createdAt: new Date(), updatedAt: new Date(),
+      },
+      {
+        masterId: '6', serviceId: '11', createdAt: new Date(), updatedAt: new Date(),
+      },
+      {
+        masterId: '6', serviceId: '12', createdAt: new Date(), updatedAt: new Date(),
+      },
+    ]);
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.bulkDelete('masterServices');
+  },
+};
